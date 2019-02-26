a95b5a07d8436db2c11f11ab0a7138ef852d1bab
Ricardo Rodrigues Tavares <ricardo.tavares@matera.com>
2019-02-26 09:53:30 -0300

    Commit inicial

---
+++ src/main/java/com/ricardortavares/netflix/examplenetflixoss/ExampleNetflixOssApplication.java
@@ -0,0 +1,13 @@
+package com.ricardortavares.netflix.examplenetflixoss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleNetflixOssApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleNetflixOssApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ricardortavares/netflix/examplenetflixoss/ExampleNetflixOssApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ricardortavares.netflix.examplenetflixoss;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ExampleNetflixOssApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
